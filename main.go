6f2c4701e3266e299983f1304a9229cbe3eb7182
hamzaanis <hamzaanis9514@gmail.com>
2019-01-09 22:06:43 +0500

    Inital File

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func readPassFile() {
+
+}
+func main() {
+	fmt.Println("Enter the name of the file to encrypt: ")
+	filename := ""
+	fmt.Scanf("%s", &filename)
+	fmt.Println(filename)
+}
