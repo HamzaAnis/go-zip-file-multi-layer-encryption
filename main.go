6a10380ada1534978b99593ad840b804bda893d6
hamzaanis <hamzaanis9514@gmail.com>
2019-01-09 22:16:32 +0500

    Reading and checking file status

--- main.go
+++ main.go
@@ -1,13 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func readPassFile() {
 
 }
+
+func file_is_exists(f string) bool {
+	_, err := os.Stat(f)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return err == nil
+}
+
+func print_error(msg string) {
+	log.Println("error: " + msg)
+}
 func main() {
 	fmt.Println("Enter the name of the file to encrypt: ")
-	filename := ""
-	fmt.Scanf("%s", &filename)
-	fmt.Println(filename)
+	inputFile := ""
+	fmt.Scanf("%s\n", &inputFile)
+	if file_is_exists(inputFile) {
+		fmt.Println("Enter the name of the password file: ")
+		passwordFile := ""
+		fmt.Scanf("%s\n", &passwordFile)
+		fmt.Println(passwordFile)
+		if file_is_exists(passwordFile) {
+			fmt.Println("Working")
+		} else {
+			print_error("file do not exist, program exiting!!")
+		}
+	} else {
+		print_error("file do not exist, program exiting!!")
+	}
 }
