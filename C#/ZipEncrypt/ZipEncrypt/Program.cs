a736625936245f6ee1856b25ca038677412742be
hamzaanis <hamzaanis9514@gmail.com>
2019-01-12 05:28:32 +0500

    Reading file content

--- C#/ZipEncrypt/ZipEncrypt/Program.cs
+++ C#/ZipEncrypt/ZipEncrypt/Program.cs
@@ -19,22 +19,35 @@ namespace ZipEncrypt
 
         private static void Main(string[] args)
         {
+            string text = "";
+            string[] passwords={};
             try
             {
-                Console.Write("Please enter the name of the password file: ");
-                var passFileName = Console.ReadLine();
-                if (!File.Exists(passFileName))
-                {
-                    printError(passFileName + " do not exits, program exiting");
-                    closeProgram();
-                }
-                Console.Write("Please enter the name of the file to encrypt: ");
+                Console.Write("Enter the name of the file to encrypt: ");
                 var textFileName = Console.ReadLine();
-                if (!File.Exists(textFileName))
+                try
+                {
+                    text = File.ReadAllText(textFileName);
+                }
+                catch (Exception e)
                 {
                     printError(textFileName + " do not exits, program exiting");
                     closeProgram();
                 }
+                Console.WriteLine(text);
+                Console.Write("Enter the name of the password file: ");
+                var passFileName = Console.ReadLine();
+                try
+                {
+                    passwords = File.ReadAllLines(passFileName);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    printError(passFileName + " do not exits, program exiting");
+                    closeProgram();
+                }
+                Console.WriteLine(passwords.Length);
             }
             catch (Exception e)
             {
@@ -48,6 +61,7 @@ namespace ZipEncrypt
             //                zip.AddFile("1.zip");
             //                zip.Save("2.zip");
             //            }
+            closeProgram();
         }
     }
 }
