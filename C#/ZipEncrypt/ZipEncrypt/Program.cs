5acf2031a29f8756b1a3fa5116e79a0994c08b24
hamzaanis <hamzaanis9514@gmail.com>
2019-01-12 05:00:42 +0500

    Inital Commit

---
+++ C#/ZipEncrypt/ZipEncrypt/Program.cs
@@ -0,0 +1,21 @@
+﻿using Ionic.Zip;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZipEncrypt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   using (ZipFile zip = new ZipFile())
+            {
+                zip.Password = "456";
+                zip.AddFile("1.zip");
+                zip.Save("2.zip");
+            }
+        }
+    }
+}
