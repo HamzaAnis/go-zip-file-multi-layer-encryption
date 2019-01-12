b70e73f4e09188c4fc46b43c45ff7ce168877dff
hamzaanis <hamzaanis9514@gmail.com>
2019-01-12 05:09:29 +0500

    Files validated

--- C#/ZipEncrypt/ZipEncrypt/Program.cs
+++ C#/ZipEncrypt/ZipEncrypt/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Ionic.Zip;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +10,40 @@ namespace ZipEncrypt
 {
     class Program
     {
+        public static void printError(string msg)
+        {
+            Console.WriteLine("error: " + msg);
+        }
+
         static void Main(string[] args)
-        {   using (ZipFile zip = new ZipFile())
+        {
+            try
             {
-                zip.Password = "456";
-                zip.AddFile("1.zip");
-                zip.Save("2.zip");
+                Console.Write("Please enter the name of the password file: ");
+                string passFileName = Console.ReadLine();
+                if (!File.Exists(passFileName))
+                {
+                    printError(passFileName + " do not exits, program exiting");
+                }
+                Console.Write("Please enter the name of the file to encrypt: ");
+                string textFileName = Console.ReadLine();
+                if (!File.Exists(passFileName))
+                {
+                    printError(textFileName + " do not exits, program exiting");
+                }
             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            //            using (ZipFile zip = new ZipFile())
+            //            {
+            //                zip.Password = "456";
+            //                zip.AddFile("1.zip");
+            //                zip.Save("2.zip");
+            //            }
         }
     }
 }
