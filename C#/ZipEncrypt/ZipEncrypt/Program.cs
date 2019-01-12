90224e5a37b67c2d4a22c0f3382a3718155b2c8a
hamzaanis <hamzaanis9514@gmail.com>
2019-01-12 05:16:33 +0500

    Exiting program sucessfulyt

--- C#/ZipEncrypt/ZipEncrypt/Program.cs
+++ C#/ZipEncrypt/ZipEncrypt/Program.cs
@@ -1,35 +1,39 @@
-﻿using Ionic.Zip;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ZipEncrypt
 {
-    class Program
+    internal class Program
     {
         public static void printError(string msg)
         {
             Console.WriteLine("error: " + msg);
         }
 
-        static void Main(string[] args)
+        public static void closeProgram()
+        {
+            Console.WriteLine("Enter any key to continue......");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+
+        private static void Main(string[] args)
         {
             try
             {
                 Console.Write("Please enter the name of the password file: ");
-                string passFileName = Console.ReadLine();
+                var passFileName = Console.ReadLine();
                 if (!File.Exists(passFileName))
                 {
                     printError(passFileName + " do not exits, program exiting");
+                    closeProgram();
                 }
                 Console.Write("Please enter the name of the file to encrypt: ");
-                string textFileName = Console.ReadLine();
-                if (!File.Exists(passFileName))
+                var textFileName = Console.ReadLine();
+                if (!File.Exists(textFileName))
                 {
                     printError(textFileName + " do not exits, program exiting");
+                    closeProgram();
                 }
             }
             catch (Exception e)
